1600a5e3e81d4130d1885d88e020e6ca20d1206a
Mhdi <0MehdiKeramati1@gmail.com>
2023-10-06 16:08:13 +0330

    responsive design

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -4,7 +4,7 @@ import Styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={Styles.Header}>
-      <h1>Coordinate Finder</h1>
+      <h1 className={Styles.title}>Coordinate Finder</h1>
       <Search />
       <Details />
     </header>
